4f85af7432f9eab231d4939956836aa351d28c34
Ricardo Montaner <ricardo.montaner@sngular.com>
2024-09-03 20:22:22 +0200

    removed unused code

--- langchain-nunsys-back/ChatOpenAI.py
+++ langchain-nunsys-back/ChatOpenAI.py
@@ -15,36 +15,3 @@ llm = ChatOpenAI(
     # organization="...",
     # other params...
 )
-
-# class OpenAI(ChatOpenAI):
-#     model_name: str = "gpt-3.5-turbo"
-#     temperature: float = 0
-#     openai_api_key: str
-#     streaming: bool = True
-
-    # class Config:
-    #     """Configuration for this pydantic object."""
-
-    #     extra = Extra.allow
-    #     arbitrary_types_allowed = True
-
-    # @staticmethod
-    # def getValidModelNames():
-    #     validModelNames = {"gpt-3.5-turbo", "gpt-3.5-turbo-16k", "gpt-4", "gpt-3.5-turbo-0613"}
-    #     return validModelNames
-      
-    # ## Validaciones  
-    # @validator("temperature")
-    # def validateTemperature(cls, request):
-    #     if request < 0 or request > 1:
-    #         raise ValueError("Temperature must be between 0 and 1")
-
-    #     return request
-
-    # @validator("model_name")
-    # def validateModelName(cls, request):
-    #     validModelNames = cls.getValidModelNames()
-
-    #     if request not in validModelNames:
-    #         raise ValueError(f"invalid model name given - {request} , valid ones are {validModelNames}")
-    #     return request
